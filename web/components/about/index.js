70e79f1e2d572efd3b76bb1c7b712c5712beb989
esbb48 <esbb48@gmail.com>
2017-01-23 15:42:04 +0800

    Fix typo

--- web/components/about/index.js
+++ web/components/about/index.js
@@ -3,4 +3,4 @@ export { default as AnnounceBoard } from './AnnounceBoard.vue';
 export { default as RevenueRate } from './RevenueRate.vue';
 export { default as Top3Salesperson } from './Top3Salesperson.vue';
 export { default as BackSeat3Salesperson } from './BackSeat3Salesperson.vue';
-export { default as TopNewsＷeekly } from './TopNewsＷeekly.vue';
+export { default as TopNewsWeekly } from './TopNewsWeekly.vue';
